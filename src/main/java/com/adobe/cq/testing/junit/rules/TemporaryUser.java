cfd168cf855d53f50b8859b446db841660ffcde3
Ramon Bisswanger <ramon@bisswanger.de>
2025-01-13 07:25:48 +0100

    update Maven plugins + dependencies (#101)

--- src/main/java/com/adobe/cq/testing/junit/rules/TemporaryUser.java
+++ src/main/java/com/adobe/cq/testing/junit/rules/TemporaryUser.java
@@ -25,7 +25,6 @@ import org.apache.sling.testing.clients.ClientException;
 import org.apache.sling.testing.clients.SlingClient;
 import org.apache.sling.testing.clients.util.poller.Polling;
 import org.junit.rules.ExternalResource;
-import org.ops4j.lang.NullArgumentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -62,7 +61,7 @@ public class TemporaryUser extends ExternalResource {
      */
     public TemporaryUser(Supplier<SlingClient> creatorSupplier, String... groups) {
         if (creatorSupplier == null) {
-            throw new NullArgumentException("creatorSupplier is null");
+            throw new IllegalArgumentException("creatorSupplier is null");
         }
 
         this.creatorSupplier = creatorSupplier;
